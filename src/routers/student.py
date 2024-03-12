8863dacdc1c6feaa5dbf723e1a63c738d4d8b5e4
leningael <leningaelrs@gmail.com>
2024-03-11 18:33:41 -0600

    fix: Solving error serializing Student

--- src/routers/student.py
+++ src/routers/student.py
@@ -1,5 +1,6 @@
 from typing import Dict
 from fastapi import APIRouter, HTTPException
+from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 from src.schemas.student import Student, StudentUpdate
 
@@ -15,25 +16,26 @@ def get_students():
 def get_student(student_id: int):
     if student_id not in students:
         return HTTPException(status_code=404, detail="Student not found")
-    return JSONResponse(content=students[student_id], status_code=200)
+    return students[student_id]
 
 @student_router.post("/")
 def create_student(student: Student):
     if student.id in students:
         return HTTPException(status_code=400, detail="Student already registered")
     students[student.id] = student
-    return JSONResponse(content=student, status_code=201)
+    return JSONResponse(content=jsonable_encoder(student), status_code=201)
 
 @student_router.put("/{student_id}")
 def update_student(student_id: int, student: StudentUpdate):
     if student_id not in students:
         return HTTPException(status_code=404, detail="Student not found")
-    students[student_id] = student
-    return JSONResponse(content=student, status_code=200)
+    updated_student = Student(id=student_id, **student.model_dump())
+    students[student_id] = updated_student
+    return updated_student
 
 @student_router.delete("/{student_id}")
 def delete_student(student_id: int):
     if student_id not in students:
         return HTTPException(status_code=404, detail="Student not found")
     students.pop(student_id)
-    return JSONResponse(content={"message": "Student deleted successfully"}, status_code=200)
+    return {"message": "Student deleted successfully"}
